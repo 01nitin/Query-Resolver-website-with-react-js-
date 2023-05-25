8698b60206f87cbe9172641d9cf511bd3c7d0e80
Nitin Pandey <116839685+01nitin@users.noreply.github.com>
2023-05-26 01:32:12 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Nitin Pandey</h1>
+            <p>@all right reserved</p>
+        </div>
+
+
+        <div>
+            <h5>follow us </h5>
+            <div>
+                <a href="https://www.youtube.com/" targer={"blank"}> Youtube </a>
+                <a href="https://www.linkedin.com/mynetwork/"targer={"blank"}> LinkedIn</a>
+                <a href="https://github.com/01nitin?tab=repositories"targer={"blank"}> Github</a>
+            </div>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
